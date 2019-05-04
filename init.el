ccc7bfce963020e9affe0b983703c6c4b51756b4
Chuck Ocheret <ocheret@panix.com>
2019-05-04 18:35:34 -0400

    Rust support.

--- init.el
+++ init.el
@@ -104,10 +104,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (go-playground go-mode))))
+ '(package-selected-packages (quote (rust-mode go-playground go-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(setq rust-format-on-save t)
